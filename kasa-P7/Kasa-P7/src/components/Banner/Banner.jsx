f5414fba22a941d813eeb1a689fbb280c9009103
merliotte <livainugent@hotmail.fr>
2023-07-01 12:19:55 +0200

    ajustement

--- kasa-P7/Kasa-P7/src/components/Banner/Banner.jsx
+++ kasa-P7/Kasa-P7/src/components/Banner/Banner.jsx
@@ -4,8 +4,10 @@ import './Banner.css'
 function Banner(props) {
     return (
         <div className='lmj-Banner'>
-            <img src= {props.imageBackground} alt =" paysage de montagne" className="lmj-Banner-image">
-            </img>
+            <div className='lmj-Banner-container'>
+                <img src= {props.imageBackground} alt =" paysage de montagne" className="lmj-Banner-image">
+                </img>
+            </div>
             <h2 className="lmj-Banner-text" >{props.textBackground}
             </h2>
         </div>
